3db07db4f22a4d9b6c866527013a6b18d28c194a
Bing O'Dowd <Bing@Bings-MacBook-Air.local>
2017-07-25 15:42:03 -0500

    Removed the js stuff

--- SimNetwrk/jak3_SimNet.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-app = flask.Flask(__name__, static_folder = 'jak3_SimNet')
-
-@app.route('/<path:path>')
-def static_proxy(path):
-    return app.send_static_file(path)
-print('Go to http://localhost:8000/jak3_SimNet.html')
-app.run(port=8000)
